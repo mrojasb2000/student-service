5d0fff6cadcdd630b8ba7c95b7eea1e5365f8ace
Mauricio Rojas <mrojasb2000@gmail.com>
2024-07-24 21:26:08 -0400

    feat: adding student model

---
+++ src/main/java/com/services/ms/student/app/domain/model/Student.java
@@ -0,0 +1,10 @@
+package com.services.ms.student.app.domain.model;
+
+
+public class Student {
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private Integer age;
+    private String address;
+}
